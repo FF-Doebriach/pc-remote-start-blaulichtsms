2b57c0022f989b539d4d67709b73b022147bdc64
root <thamersimon@gmail.com>
2023-07-07 14:30:48 +0200

    added timeout to request

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from decouple import config
 
 if __name__ == "__main__":
     root_path = config("ROOT_PATH", default="")
-    uvicorn.run("src.api:app", host="0.0.0.0", port=8080, reload=True, root_path=root_path)
+    uvicorn.run("src.api:app", host="0.0.0.0", port=8080,
+                reload=True, root_path=root_path)
--- src/blaulichtsms.py
+++ src/blaulichtsms.py
@@ -2,11 +2,13 @@ import requests
 from src.models import *
 import pytz
 import datetime
+from src.utils import Utilities
 
 
 BASE_URL = "https://api.blaulichtsms.net/blaulicht/api/"
 ALARM_URL = BASE_URL + "alarm/v1/dashboard/"
 utc = pytz.UTC
+logger = Utilities.setup_logger()
 
 
 class BlaulichtSMSAPI:
@@ -14,14 +16,18 @@ class BlaulichtSMSAPI:
         self._token = token
 
     def get_alarm_state(self) -> BlaulichtSMSAPIAlarmResponse:
-        req = requests.get(ALARM_URL+self._token)
-        alarm_list = list()
-        if req.status_code == 200:
-            for c_alarm in req.json()["alarms"]:
-                localized_time = utc.localize(datetime.datetime.strptime(
-                    c_alarm["alarmDate"], "%Y-%m-%dT%H:%M:%S.%fZ"))
-                alarm_list.append(BlaulichtSMSAPIAlarmSingleResponse(
-                    alarmDate=localized_time, alarmText=c_alarm["alarmText"]))
-        else:
-            print(f"Status code: {req.status_code}")
+        try:
+            req = requests.get(ALARM_URL+self._token, timeout=10)
+            alarm_list = list()
+            if req.status_code == 200:
+                for c_alarm in req.json()["alarms"]:
+                    localized_time = utc.localize(datetime.datetime.strptime(
+                        c_alarm["alarmDate"], "%Y-%m-%dT%H:%M:%S.%fZ"))
+                    alarm_list.append(BlaulichtSMSAPIAlarmSingleResponse(
+                        alarmDate=localized_time, alarmText=c_alarm["alarmText"]))
+            else:
+                print(f"Status code: {req.status_code}")
+        except requests.exceptions.Timeout:
+            logger.error("Timed out!")
+            return BlaulichtSMSAPIAlarmResponse(alarms=[])
         return BlaulichtSMSAPIAlarmResponse(alarms=alarm_list)
